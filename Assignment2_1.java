155ed5a1a2c4f96866c513c60bcc0198947fbb0f
rajanku2512 <rajanku2512@gmail.com>
2016-12-27 23:27:42 +0530

    Add files via upload

---
+++ Assignment2_1.java
@@ -0,0 +1,13 @@
+package programmes;
+
+public class Assignment2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+if(System.out.printf("Rajan kumar")==null){
+}
+}
+
+	}
+
+
